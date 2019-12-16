7edf59f3c71c02dd02b64173877f039be318163e
Justina Kuchta <justyna.kuchta@gmail.com>
2019-12-16 09:45:23 -0500

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -5,24 +5,23 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 export default class ChromeBoisDomain extends Component {
   
   handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
+    /* should capture the `x` and `y` coordinates of the mouse
      * from the event and use them to invoke the `drawChromeBoiAtCoords`
      * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
+
     let x = event.clientX
     let y = event.clientY
     drawChromeBoiAtCoords(x, y)
   }
-
-  // handleMouseMove = (event) => {
-  //   drawChromeBoiAtCoords(event.clientX, event.clientY)
-  // }
   
-  /* TODO: Create an event handler which, when fired, invokes the provided
-   * `toggleCycling` function with no arguments. Don't forget the click event
-   * listener that should fire it!
+  /* TODO: Add an event listener to the `<canvas>` element to capture when a key
+  /* is pressed. When a key is pressed, an event handler should invoke the the
+  /* provided `resize` function with a single argument of either '+' or '-'
+  /* if the key pressed was 'a', then it should call `resize` with '+'
+  /* if the key pressed was 's', then it should call `resize` with '-' 
    */
+
   handleKeyDown = (event) => {
     if(event.key === "a") {
       resize('+')
@@ -32,11 +31,9 @@ export default class ChromeBoisDomain extends Component {
     }
   }
    
-  /* TODO: Add an event listener to the `<canvas>` element to capture when a key
-  /* is pressed. When a key is pressed, an event handler should invoke the the
-  /* provided `resize` function with a single argument of either '+' or '-'
-  /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-' 
+   /* Create an event handler which, when fired, invokes the provided
+   * `toggleCycling` function with no arguments. Don't forget the click event
+   * listener that should fire it!
    */
   
   render() {
@@ -44,7 +41,8 @@ export default class ChromeBoisDomain extends Component {
       <canvas 
         onMouseMove={this.handleMouseMove}
         onKeyDown={this.handleKeyDown}
-        onClick={toggleCycling}
+        onClick={toggleCycling} //OR: onClick={() => {toggleCycling()}}
+        // no arguments required, that is why no this
         width='900'
         height='600'
         tabIndex="0">
